d18fcb4a52031382dbebfe5f65aa7e4a4f1b52fa
drobert-bfm <drobert@bfm.bm>
2017-04-27 12:15:21 -0300

    Added REST resource

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Random;
 
 public class InsultGenerator {
 	public String generateInsult() {
